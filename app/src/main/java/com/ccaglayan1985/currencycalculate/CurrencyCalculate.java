3ee2bf64e60f4073b896a021843188608bb3dbfa
cihan caglayan <ccaglayan1985@gmail.com>
2017-03-19 01:43:46 +0300

    İlk ekrana componentler eklendi.

--- app/src/main/java/com/ccaglayan1985/currencycalculate/CurrencyCalculate.java
+++ app/src/main/java/com/ccaglayan1985/currencycalculate/CurrencyCalculate.java
@@ -1,7 +1,16 @@
 package com.ccaglayan1985.currencycalculate;
 
-import android.support.v7.app.AppCompatActivity;
+
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import static com.ccaglayan1985.currencycalculate.R.array.islem_tipi;
+
 
 public class CurrencyCalculate extends AppCompatActivity {
 
@@ -9,5 +18,31 @@ public class CurrencyCalculate extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_currency_calculate);
+
+        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+        final String islemtipi[] = getResources().getStringArray(islem_tipi);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getApplicationContext(), android.R.layout.activity_list_item, islemtipi);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getApplicationContext(), "seçilen işlem tipi" + islemtipi[position], Toast.LENGTH_SHORT).show();
+
+                switch (position) {
+
+
+                }
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
     }
 }
+
+
